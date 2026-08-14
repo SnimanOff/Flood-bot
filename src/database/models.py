a7f4d0097c65233e307857b5b752ff4318be36e2
SnimanOff <sidorin.vadim.08@gmail.com>
2026-08-14 13:45:05 +0300

    add balance into users model

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import BigInteger, String
+from sqlalchemy import BigInteger, String, Integer
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,6 +10,8 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    
+
     tg_id: Mapped[int] = mapped_column(BigInteger, unique=True, index=True)
     username: Mapped[str | None] = mapped_column(String(255), nullable=True)
+
+    balance: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
