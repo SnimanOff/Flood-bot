c164f8a300c849d83c53d31f957e79812c2c1caf
SnimanOff <sidorin.vadim.08@gmail.com>
2026-08-14 12:25:03 +0300

    fix settings

--- src/service/settings.py
+++ src/service/settings.py
@@ -7,15 +7,15 @@ BASE_DIR = Path(__file__).resolve().parents[2]
 
 
 class Settings(BaseSettings):
+
+    database: str
+    bot: str
+
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
-
-    database: str = "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres"
-    log_level: str = "INFO"
-
 
 @lru_cache
 def get_settings() -> Settings:
